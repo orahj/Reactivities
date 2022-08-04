023b876996ee9e6489309b7716ccd53ea89f6796
Isaac Orah <isaac.orah@tek-experts.com>
2022-08-04 13:20:07 +0300

    all Comits

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,46 @@
+﻿
+using Application.Activities;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await Mediator.Send(new List.Query());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await Mediator.Send(new Details.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity([FromBody]Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> DeleteActivity(Guid Id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command { Id = Id }));
+        }
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,18 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
